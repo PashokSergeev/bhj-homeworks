4abbe2db225da456bda8754f7f6f9d861f0047ad
Pavel <pavel_sergeev_2003@bk.ru>
2025-04-07 19:41:11 +0300

    Создание Cookie-clicker 1 уровня

---
+++ js-features/cookie-clicker/task.js
@@ -0,0 +1,9 @@
+const cookie = document.getElementById('cookie');
+cookie.onclick = () => {
+    const clickerCounter = document.getElementById('clicker__counter');
+    clickerCounter.textContent = parseInt(clickerCounter.textContent) + 1;
+    cookie.width += 20;
+    setTimeout(() => {
+        cookie.width -= 20;
+    }, 100);
+}
